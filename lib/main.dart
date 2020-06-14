b5a5e6cbdfcb2e46963851c7d0f16f0b4bcfa6f7
junior <josejuniordsc@gmail.com>
2020-06-13 23:13:39 -0300

    initialized services screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:Fique_Bem/scenes/places/servicesScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'theme/themes.dart';
-void main() => runApp(App());
+
+void main() => runApp(MaterialApp(
+    routes: {
+          '/services': (context) => ServicesScreen(),
+    },
+    home: App()
+));
 
 class App extends StatefulWidget {
   @override
@@ -11,8 +18,6 @@ class App extends StatefulWidget {
 }
 
 class AppState extends State<App> {
-  // Private variable uses _ before the name
-
   @override
   void initState() {
     super.initState();
@@ -20,8 +25,7 @@ class AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
+    return new Scaffold(
         // App Bar
         appBar: AppBar(
           backgroundColor: Colors.grey[300],
@@ -74,26 +78,27 @@ class AppState extends State<App> {
         ),
 
         // Body
-        body: RecentPayments(),
+        body: Text("wherever"),
 
         // Bottom
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: Container(
           padding: EdgeInsets.only(top: 30),
-           height: 50,
-           width: 60,
-           child: new FloatingActionButton(
+          height: 50,
+          width: 60,
+          child: new FloatingActionButton(
               backgroundColor: themeData.canvasColor,
               onPressed: () {},
               child: Container(
-                  child: ConstrainedBox(
-                      constraints: BoxConstraints.expand(),
-                      child: FlatButton(
-                          onPressed: null,
-                          padding: EdgeInsets.all(0.0),
-                          child: SvgPicture.asset('assets/icons/awesome-road.svg'),),),)
-
-              ),
+                child: ConstrainedBox(
+                  constraints: BoxConstraints.expand(),
+                  child: FlatButton(
+                    onPressed: null,
+                    padding: EdgeInsets.all(0.0),
+                    child: SvgPicture.asset('assets/icons/awesome-road.svg'),
+                  ),
+                ),
+              )),
         ),
 
         bottomNavigationBar: BottomAppBar(
@@ -127,22 +132,18 @@ class AppState extends State<App> {
                   width: 70,
                   child: FlatButton(
                       onPressed: () {
-                        print("Hello world 1");
+                       Navigator.of(context).pushNamed('/services');
                       },
                       child: Column(
                         children: <Widget>[
                           Container(
                             width: 40,
                             height: 30,
-                         
-                            child:SvgPicture.asset(
-                            'assets/icons/Explore.svg',
-                           color: themeData.primaryColor,
+                            child: SvgPicture.asset(
+                              'assets/icons/Explore.svg',
+                              color: themeData.primaryColor,
+                            ),
                           ),
-                           
-                          
-                          ),
-                      
                           Text(
                             "Serviços",
                             style: themeData.textTheme.bodyText1,
@@ -161,13 +162,14 @@ class AppState extends State<App> {
                       },
                       child: Column(
                         children: <Widget>[
-                           Container(
+                          Container(
                             width: 40,
                             height: 30,
-                             child:SvgPicture.asset(
-                            'assets/icons/Icon awesome-bell.svg',
-                           color: themeData.primaryColor,
-                          ),),
+                            child: SvgPicture.asset(
+                              'assets/icons/Icon awesome-bell.svg',
+                              color: themeData.primaryColor,
+                            ),
+                          ),
                           Text(
                             "Notificações",
                             style: themeData.textTheme.bodyText1,
@@ -200,254 +202,6 @@ class AppState extends State<App> {
             color: Colors.white,
           ),
         ),
-      ),
-    );
-  }
-}
-
-class RecentPayments extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Color.fromARGB(255, 241, 241, 241),
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 120,
-            color: themeData.primaryColor,
-            width: MediaQuery.of(context).size.width,
-            child: Container(
-              margin: EdgeInsets.only(top: 10, left: 15),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.topLeft,
-                    padding: EdgeInsets.only(bottom: 10),
-                    child: Text(
-                      "Sugestões para você",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w800,
-                        fontSize: 14,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          Container(
-                            width: 60.0,
-                            height: 60.0,
-                            decoration: BoxDecoration(
-                                border:
-                                    Border.all(width: 2, color: Colors.white),
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                  fit: BoxFit.fill,
-                                  image: NetworkImage(
-                                    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Grupo_CCR.svg/1200px-Grupo_CCR.svg.png",
-                                  ),
-                                )),
-                          ),
-                          Text(
-                            "@CCR",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ],
-                      ),
-                      Spacer(),
-                      Column(
-                        children: <Widget>[
-                          Container(
-                            width: 60.0,
-                            height: 60.0,
-                            decoration: BoxDecoration(
-                                border:
-                                    Border.all(width: 2, color: Colors.white),
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                  fit: BoxFit.fill,
-                                  image: NetworkImage(
-                                    "https://www.picpay.com/static/images/new/home/ppay-icon.png",
-                                  ),
-                                )),
-                          ),
-                          Text(
-                            "@thiago.s",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ],
-                      ),
-                      Spacer(),
-                      Column(
-                        children: <Widget>[
-                          Container(
-                            width: 60.0,
-                            height: 60.0,
-                            decoration: BoxDecoration(
-                                border:
-                                    Border.all(width: 2, color: Colors.white),
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                  fit: BoxFit.fill,
-                                  image: NetworkImage(
-                                    "https://www.picpay.com/static/images/new/home/ppay-icon.png",
-                                  ),
-                                )),
-                          ),
-                          Text(
-                            "@heymarina",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ],
-                      ),
-                      Spacer(),
-                      Column(
-                        children: <Widget>[
-                          Container(
-                            width: 60.0,
-                            height: 60.0,
-                            decoration: BoxDecoration(
-                                border:
-                                    Border.all(width: 2, color: Colors.white),
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                  fit: BoxFit.fill,
-                                  image: NetworkImage(
-                                    "https://www.picpay.com/static/images/new/home/ppay-icon.png",
-                                  ),
-                                )),
-                          ),
-                          Text(
-                            "Cielo",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 10),
-            child: Container(
-              padding: EdgeInsets.only(left: 10, right: 10, top: 10),
-              width: MediaQuery.of(context).size.width,
-              height: 30,
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    "Atividades",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w800,
-                    ),
-                  ),
-                  Spacer(),
-                  Container(
-                    width: 60,
-                    child: Text(
-                      "Todas",
-                      style: TextStyle(
-                          fontSize: 14,
-                          color: Color.fromARGB(255, 44, 207, 131),
-                          fontWeight: FontWeight.w800),
-                    ),
-                  ),
-                  Container(
-                    width: 50,
-                    child: Text("Minhas",
-                        style: TextStyle(
-                            fontSize: 14,
-                            color: Color.fromARGB(255, 146, 148, 156),
-                            fontWeight: FontWeight.w800)),
-                  )
-                ],
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(7.0),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.all(16.0),
-                  color: Colors.white,
-                  height: 150,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(
-                        children: <Widget>[
-                          Container(
-                            width: 45.0,
-                            height: 45.0,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                  fit: BoxFit.fill,
-                                  image: NetworkImage(
-                                    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Grupo_CCR.svg/1200px-Grupo_CCR.svg.png",
-                                  ),
-                                )),
-                          ),
-                          Text(
-                            " @CCR",
-                            style: TextStyle(fontWeight: FontWeight.w800),
-                          ),
-                          Text(" pagou "),
-                          Text(
-                            "Você",
-                            style: TextStyle(fontWeight: FontWeight.w800),
-                          ),
-                        ],
-                      ),
-                      Spacer(),
-                      Row(
-                        children: <Widget>[
-                          Text("Bebeu Água ontem a noite! 🚰"),
-                        ],
-                      ),
-                      Spacer(),
-                      Row(
-                        children: <Widget>[
-                          Text(
-                            "R\$ 10,00",
-                            style: TextStyle(
-                                color: Color.fromARGB(255, 44, 207, 131),
-                                fontWeight: FontWeight.w800),
-                          ),
-                          Text(" | "),
-                          Icon(
-                            Icons.people_outline,
-                            color: Colors.grey,
-                            size: 20,
-                          ),
-                          Text(
-                            " 4 minutos",
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                          Spacer(),
-                          Icon(Icons.chat_bubble_outline),
-                          Text("   3   "),
-                          Icon(
-                            Icons.favorite,
-                            color: Color.fromARGB(255, 44, 207, 131),
-                          ),
-                          Text("   5   "),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                new Divider()
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/widgets/partner_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PartnerWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return (
+      Text("Parceiros")
+    );
+  }
+}
