bfcc96db56e42207dc4c5a7ab87e6f884948f78b
daanilo-s19 <38538568+Daanilo-s19@users.noreply.github.com>
2020-06-13 22:59:34 -0300

    fix: bottom bar

--- lib/Dashboard/Dashboard.dart
+++ lib/Dashboard/Dashboard.dart
@@ -1,4 +1,6 @@
+import 'package:Fique_Bem/SplashScreen/SplashScreen.dart';
 import 'package:Fique_Bem/Widgets/AppBar_widget.dart';
+import 'package:Fique_Bem/Widgets/Bottom_navigation_widget.dart';
 import 'package:Fique_Bem/theme/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -11,136 +13,56 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [
+    Container(
+      child: Text('tela 1'),
+    ),
+    Container(
+      child: Text('tela 2'),
+    ),
+    Container(
+      child: Text('tela 3'),
+    ),
+    Container(
+      child: Text('tela 4'),
+    ),
+    Container(
+      child: Text('tela 5'),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:
-          AppBar(backgroundColor: Color(0xFF04BFBF), title: AppbarNavigation()),
-      body: Text('entrei'),
+        appBar: AppBar(
+            backgroundColor: Color(0xFF04BFBF), title: AppbarNavigation()),
+        body: _children[_currentIndex],
 
-      // Bottom
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Container(
-        padding: EdgeInsets.only(top: 30),
-        height: 50,
-        width: 60,
-        child: new FloatingActionButton(
-            backgroundColor: themeData.canvasColor,
+        // Bottom
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: Container(
+          width: 80,
+          height: 80,
+          child: FloatingActionButton(
+            backgroundColor: Color(0xFF0C9494),
             onPressed: () {},
-            child: Container(
-              child: ConstrainedBox(
-                constraints: BoxConstraints.expand(),
-                child: FlatButton(
-                  color: themeData.canvasColor,
-                  onPressed: null,
-                  padding: EdgeInsets.all(0.0),
-                  child: SvgPicture.asset('assets/icons/awesome-road.svg'),
-                ),
-              ),
-            )),
-      ),
-
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 55.0,
-          padding: EdgeInsets.only(top: 5),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                width: 70,
-                child: FlatButton(
-                    onPressed: () {
-                      print("Hello world 1");
-                    },
-                    child: Column(
-                      children: <Widget>[
-                        Icon(
-                          Icons.home,
-                          size: 30,
-                          color: themeData.primaryColor,
-                        ),
-                        Text(
-                          "Início",
-                          style: themeData.textTheme.bodyText1,
-                        )
-                      ],
-                    )),
-              ),
-              Container(
-                width: 70,
-                child: FlatButton(
-                    onPressed: () {
-                      print("Hello world 1");
-                    },
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          width: 20,
-                          height: 30,
-                          child: SvgPicture.asset(
-                            'assets/icons/Explore.svg',
-                            color: themeData.primaryColor,
-                          ),
-                        ),
-                        Text(
-                          "Serviços",
-                          style: themeData.textTheme.bodyText1,
-                        )
-                      ],
-                    )),
-              ),
-              Container(
-                width: 75,
-              ),
-              Container(
-                width: 80,
-                child: FlatButton(
-                    onPressed: () {
-                      print("Hello world 1");
-                    },
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          width: 20,
-                          height: 30,
-                          child: SvgPicture.asset(
-                            'assets/icons/Icon awesome-bell.svg',
-                            color: themeData.primaryColor,
-                          ),
-                        ),
-                        Text(
-                          "Notificações",
-                          style: themeData.textTheme.bodyText1,
-                        )
-                      ],
-                    )),
-              ),
-              Container(
-                width: 60,
-                child: FlatButton(
-                    onPressed: () {
-                      print("Hello world 1");
-                    },
-                    child: Column(
-                      children: <Widget>[
-                        Icon(
-                          Icons.settings,
-                          size: 30,
-                          color: themeData.primaryColor,
-                        ),
-                        Text(
-                          "Ajustes",
-                          style: TextStyle(fontSize: 8),
-                        )
-                      ],
-                    )),
-              ),
-            ],
+            child: SvgPicture.asset(
+              "assets/icons/logo.svg",
+              color: Colors.white,
+              width: 37.17,
+              height: 28.11,
+            ),
           ),
-          color: Colors.white,
         ),
-      ),
-    );
+        bottomNavigationBar:
+            BottomNavBar(onTap: handleIndexBottom, index: _currentIndex));
+  }
+
+  void handleIndexBottom(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
